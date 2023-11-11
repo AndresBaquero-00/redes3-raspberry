6bd9cfc0f581041bb7727f420377cbf9c8565d34
AndresBaquero-00 <andres.baquero6@gmail.com>
2023-11-11 16:27:27 -0500

    Arreglado error en 11.

--- send_data.py
+++ send_data.py
@@ -8,7 +8,7 @@ from weather_aq_data import get_weather_aq_data
 load_dotenv()
 
 API_URL = os.getenv('API_HOST') + '/api/v2'
-data = {**get_data(), **get_weather_aq_data}
+data = {**get_data(), **get_weather_aq_data()}
 
 res = requests.post(f'{API_URL}/save', data, headers={'Content-Type': 'application/json'})
 print(res.json())
