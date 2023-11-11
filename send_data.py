a4c9691663338d55ad7b5d7dbef1362c45b45b03
AndresBaquero-00 <andres.baquero6@gmail.com>
2023-11-11 17:03:45 -0500

    Añadida encriptación de datos mediante AES.

--- send_data.py
+++ send_data.py
@@ -1,5 +1,8 @@
+import json
 import os
 import requests
+from Crypto.Cipher import AES
+from Crypto.Util import Padding
 from dotenv import load_dotenv
 
 from mq135 import get_data
@@ -10,5 +13,12 @@ load_dotenv()
 API_URL = os.getenv('API_HOST') + '/api/v2'
 data = {**get_data(), **get_weather_aq_data()}
 
+key = os.getenv('CRYPTO_KEY')
+cipher = AES.new(bytes.fromhex(key), AES.MODE_ECB)
+encoded = cipher.encrypt(Padding.pad(json.dumps(data).encode(), 16))
+
 res = requests.post(f'{API_URL}/save', json=data, headers={'Content-Type': 'application/json'})
+print(res.content)
+
+res = requests.post(f'{API_URL}/save-secure', data=encoded, headers={'Content-Type': 'text/plain'})
 print(res.content)
