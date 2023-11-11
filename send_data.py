8782f714a7f1918c20b734e570ef680ef09995ea
AndresBaquero-00 <andres.baquero6@gmail.com>
2023-11-11 16:33:50 -0500

    Enviando datos en json.

--- send_data.py
+++ send_data.py
@@ -10,5 +10,5 @@ load_dotenv()
 API_URL = os.getenv('API_HOST') + '/api/v2'
 data = {**get_data(), **get_weather_aq_data()}
 
-res = requests.post(f'{API_URL}/save', data, headers={'Content-Type': 'application/json'})
+res = requests.post(f'{API_URL}/save', json=data, headers={'Content-Type': 'application/json'})
 print(res.content)
